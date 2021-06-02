9d96374111f573e8b01673d026660227e87f06a2
jonytalukdar <jonytalukdar30@gmail.com>
2021-06-02 22:24:09 +0600

    added modal

--- client/src/Components/Appointment/BookingCard/BookingCard.js
+++ client/src/Components/Appointment/BookingCard/BookingCard.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AppointmentForm from '../AppointmentForm/AppointmentForm';
 
 const BookingCard = ({ booking }) => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <div className="col-md-4 mb-5">
       <div className="card p-3">
@@ -8,9 +18,14 @@ const BookingCard = ({ booking }) => {
           <h5 className="card-title text-brand">{booking.subject}</h5>
           <h6>{booking.visitingHour}</h6>
           <p>{booking.totalSpace} SPACES AVAILABLE</p>
-          <button className="btn btn-brand text-uppercase">
+          <button onClick={openModal} className="btn btn-brand text-uppercase">
             Book Appointment
           </button>
+          <AppointmentForm
+            modalIsOpen={modalIsOpen}
+            closeModal={closeModal}
+            appointmentOn={booking.subject}
+          ></AppointmentForm>
         </div>
       </div>
     </div>
