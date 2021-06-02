4325ef1e4e4f02ed1311f25a813282d3ed8132c8
jonytalukdar <jonytalukdar30@gmail.com>
2021-06-02 22:49:45 +0600

    fixed react hook form

--- client/src/Components/Appointment/AppointmentForm/AppointmentForm.js
+++ client/src/Components/Appointment/AppointmentForm/AppointmentForm.js
@@ -32,17 +32,100 @@ const AppointmentForm = ({ modalIsOpen, closeModal, appointmentOn }) => {
         style={customStyles}
         contentLabel="Example Modal"
       >
-        <h2 className="text-brand text-center">{appointmentOn}</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          {/* register your input into the hook by invoking the "register" function */}
-          <input defaultValue="test" {...register('example')} />
+        <h2 className="text-center text-brand">{appointmentOn}</h2>
+        <p className="text-secondary text-center">
+          {/* <small>ON {date.toDateString()}</small> */}
+        </p>
+        <form className="p-5" onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-group">
+            <input
+              type="text"
+              {...register('yourName', { required: true })}
+              name="name"
+              placeholder="Your Name"
+              className="form-control"
+            />
+            {errors.name && (
+              <span className="text-danger">This field is required</span>
+            )}
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              {...register('phoneNumber', { required: true })}
+              name="phone"
+              placeholder="Phone Number"
+              className="form-control"
+            />
+            {errors.phone && (
+              <span className="text-danger">This field is required</span>
+            )}
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              //   ref={register({ required: true })}
+              {...register('email', { required: true })}
+              name="email"
+              placeholder="Email"
+              className="form-control"
+            />
+            {errors.email && (
+              <span className="text-danger">This field is required</span>
+            )}
+          </div>
+          <div className="form-group row">
+            <div className="col-4">
+              <select
+                className="form-control"
+                name="gender"
+                // ref={register({ required: true })}
+                {...register('gender', { required: true })}
+              >
+                <option disabled={true} value="Not set">
+                  Select Gender
+                </option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Not set">Other</option>
+              </select>
+              {errors.gender && (
+                <span className="text-danger">This field is required</span>
+              )}
+            </div>
+            <div className="col-4">
+              <input
+                // ref={register({ required: true })}
+                {...register('age', { required: true })}
+                className="form-control"
+                name="age"
+                placeholder="Your Age"
+                type="number"
+              />
+              {errors.age && (
+                <span className="text-danger">This field is required</span>
+              )}
+            </div>
+            <div className="col-4">
+              <input
+                // ref={register({ required: true })}
+                {...register('weight', { required: true })}
+                className="form-control"
+                name="weight"
+                placeholder="Weight"
+                type="number"
+              />
+              {errors.weight && (
+                <span className="text-danger">This field is required</span>
+              )}
+            </div>
+          </div>
 
-          {/* include validation with required or other standard HTML validation rules */}
-          <input {...register('exampleRequired', { required: true })} />
-          {/* errors will return when field validation fails  */}
-          {errors.exampleRequired && <span>This field is required</span>}
-
-          <input type="submit" />
+          <div className="form-group text-right">
+            <button type="submit" className="btn btn-brand">
+              Send
+            </button>
+          </div>
         </form>
       </Modal>
     </div>
