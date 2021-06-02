b0c5dbc45760d7a2f2722fae732e95e2b8897919
jonytalukdar <jonytalukdar30@gmail.com>
2021-06-02 19:19:46 +0600

    complete make appointment section

--- client/src/Components/Home/Home/Home.js
+++ client/src/Components/Home/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FeaturesServices from '../FeaturesServices.js/FeaturesServices';
 import Header from '../Header/Header';
+import MakeAppoinment from '../MakeAppoinment/MakeAppointment';
 import Services from '../Services/Services';
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
       <Header />
       <Services />
       <FeaturesServices />
+      <MakeAppoinment />
     </div>
   );
 };
