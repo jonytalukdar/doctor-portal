3faa9ee955718c784fcd68583ba815e29690cf2c
jonytalukdar <jonytalukdar30@gmail.com>
2021-06-03 22:25:37 +0600

    change link

--- client/src/Components/AllPatients/AllPatients/AllPatients.js
+++ client/src/Components/AllPatients/AllPatients/AllPatients.js
@@ -3,23 +3,26 @@ import AppointmentDataTable from '../../Dashboard/AppointmentDataTable/Appointme
 import Sidebar from '../../Dashboard/Sidebar/Sidebar';
 
 const AllPatients = () => {
-    const [appointments, setAppointments] = useState([]);
+  const [appointments, setAppointments] = useState([]);
 
-    useEffect(() => {
-        fetch('https://salty-plateau-71286.herokuapp.com/appointments')
-            .then(res => res.json())
-            .then(data => setAppointments(data))
-    }, [])
+  useEffect(() => {
+    fetch('http://localhost:5000/appointments')
+      .then((res) => res.json())
+      .then((data) => setAppointments(data));
+  }, []);
 
-    return (
-        <div className="container-fluid row" >
-            <Sidebar></Sidebar>
-            <div className="col-md-10 p-4 pr-5" style={{ position: "absolute", right: 0, backgroundColor: "#F4FDFB" }}>
-                <h5 className="text-brand">All Patients</h5>
-                <AppointmentDataTable appointments={appointments} />
-            </div>
-        </div>
-    );
+  return (
+    <div className="container-fluid row">
+      <Sidebar></Sidebar>
+      <div
+        className="col-md-10 p-4 pr-5"
+        style={{ position: 'absolute', right: 0, backgroundColor: '#F4FDFB' }}
+      >
+        <h5 className="text-brand">All Patients</h5>
+        <AppointmentDataTable appointments={appointments} />
+      </div>
+    </div>
+  );
 };
 
 export default AllPatients;
--- client/src/Components/Dashboard/Dashboard/Dashboard.js
+++ client/src/Components/Dashboard/Dashboard/Dashboard.js
@@ -6,47 +6,44 @@ import 'react-calendar/dist/Calendar.css';
 import { UserContext } from '../../../App';
 
 const containerStyle = {
-    backgroundColor: "#F4FDFB",
-    border: '1px solid red'
-}
+  backgroundColor: '#F4FDFB',
+  border: '1px solid red',
+};
 
 const Dashboard = () => {
-    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
-    const [selectedDate, setSelectedDate] = useState(new Date());
-    const [appointments, setAppointments] = useState([]);
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [appointments, setAppointments] = useState([]);
 
-    const handleDateChange = date => {
-        setSelectedDate(date);
-    }
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
 
-    useEffect(() => {
-        fetch('https://salty-plateau-71286.herokuapp.com/appointmentsByDate', {
-            method: 'POST',
-            headers: { 'content-type': 'application/json' },
-            body: JSON.stringify({ date: selectedDate, email: loggedInUser.email })
-        })
-            .then(res => res.json())
-            .then(data => setAppointments(data))
-    }, [selectedDate])
+  useEffect(() => {
+    fetch('http://localhost:5000/appointmentsByDate', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ date: selectedDate, email: loggedInUser.email }),
+    })
+      .then((res) => res.json())
+      .then((data) => setAppointments(data));
+  }, [selectedDate]);
 
-    return (
-        <section>
-            <div style={containerStyle} className="row">
-                <div className="col-md-2 col-sm-6 col-12">
-                    <Sidebar></Sidebar>
-                </div>
-                <div className="col-md-5 col-sm-12 col-12 d-flex justify-content-center">
-                    <Calendar
-                        onChange={handleDateChange}
-                        value={new Date()}
-                    />
-                </div>
-                <div className="col-md-5 col-sm-12 col-12">
-                    <AppointmentsByDate appointments={appointments}></AppointmentsByDate>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section>
+      <div style={containerStyle} className="row">
+        <div className="col-md-2 col-sm-6 col-12">
+          <Sidebar></Sidebar>
+        </div>
+        <div className="col-md-5 col-sm-12 col-12 d-flex justify-content-center">
+          <Calendar onChange={handleDateChange} value={new Date()} />
+        </div>
+        <div className="col-md-5 col-sm-12 col-12">
+          <AppointmentsByDate appointments={appointments}></AppointmentsByDate>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Dashboard;
