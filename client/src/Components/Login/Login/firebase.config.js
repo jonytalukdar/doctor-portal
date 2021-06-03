61f61456c7f06ea1398754a376dc41566dddbab8
jonytalukdar <jonytalukdar30@gmail.com>
2021-06-03 22:17:47 +0600

    added databse

--- client/src/Components/Login/Login/firebase.config.js
+++ client/src/Components/Login/Login/firebase.config.js
@@ -1,11 +1,10 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBTiD5rATxd0_NNaAsIXiJmIHN_O8akO3o",
-  authDomain: "doctors-portal-ph.firebaseapp.com",
-  databaseURL: "https://doctors-portal-ph.firebaseio.com",
-  projectId: "doctors-portal-ph",
-  storageBucket: "doctors-portal-ph.appspot.com",
-  messagingSenderId: "7156633348",
-  appId: "1:7156633348:web:08a1f9dd6293224e390a51",
-  measurementId: "G-FMZXNF8K2E"
+var firebaseConfig = {
+  apiKey: 'AIzaSyAx2l4eS_hsqzyVIteEXsSiCe7pgBASChk',
+  authDomain: 'doctor-portal-29053.firebaseapp.com',
+  projectId: 'doctor-portal-29053',
+  storageBucket: 'doctor-portal-29053.appspot.com',
+  messagingSenderId: '265864877329',
+  appId: '1:265864877329:web:e9d32bf9446b455117a8b0',
 };
-  export default firebaseConfig;
+
+export default firebaseConfig;
